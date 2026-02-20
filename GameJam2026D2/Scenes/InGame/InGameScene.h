540a3d3bec64dd69c878ada63b846266debf3fea
wataru0713 <wataru-tokumine-k24h@stu.kbc.ac.jp>
2026-02-20 15:00:40 +0900

    InGmae.h

--- GameJam2026D2/Scenes/InGame/InGameScene.h
+++ GameJam2026D2/Scenes/InGame/InGameScene.h
@@ -1,1 +1,34 @@
 #pragma once
+
+#include "SceneBase.h"
+#include "../Object/Player/Player.h"
+
+class InGameScene :public SceneBase
+{
+private:
+	int wallHeight;
+	int wallTop;
+	Player* player;
+
+	//BGM
+	Int InGameBGM = -1;
+
+	//ƒtƒHƒ“ƒg
+	int timerFont;
+	int titleFont;
+
+public:
+	InGameScene();
+	virtual ~InGameScene();
+
+private:
+	virtual void Initialize() override;
+	virtual eSceneType Update() override;
+	virtual void Draw() const override;
+	virtual void Finalize() override;
+
+	//
+
+public:
+	virtual eSceneType::eInGame;
+};
