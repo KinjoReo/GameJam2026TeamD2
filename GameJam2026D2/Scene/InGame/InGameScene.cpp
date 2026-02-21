c93cd2cadbfcaafdd1c7f75d94a29e3a220b269c
wataru0713 <wataru-tokumine-k24h@stu.kbc.ac.jp>
2026-02-21 13:39:57 +0900

    a

---
+++ GameJam2026D2/Scene/InGame/InGameScene.cpp
@@ -0,0 +1,15 @@
+#include "InGameScene.h"
+
+#include "DxLib.h"
+
+InGameScene::InGameScene()
+	:player(nullptr)
+{
+}
+
+void InGameScene::Initialize()
+{
+	AssetsContainer* container = AssetsContainer::GetInstance();
+
+
+}
---
+++ GameJam2026D2/Scene/InGame/InGameScene.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class InGameScene : public SceneBase
+{
+private:
+	int wallHeight;
+	int wallTop;
+	Player* player;
+
+	//BGM
+
+
+};
